de3777b169e72e9bc9d21d986470e428e9c28cec
Christophe Grand <christophe@cgrand.net>
2017-04-26 13:27:30 +0200

    borrowing ouroboros from powderkeg

---
+++ src/main/java/portkey/Agent.java
@@ -0,0 +1,14 @@
+package portkey;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+    static {
+        if (Agent.class.getClassLoader() != ClassLoader.getSystemClassLoader())
+            throw new IllegalStateException("This class is expected to be loaded by the system classloader.");
+    }
+	public static Instrumentation instrumentation;
+	public static void agentmain(String args, Instrumentation instrumentation) {
+		Agent.instrumentation = instrumentation;
+	}
+}
